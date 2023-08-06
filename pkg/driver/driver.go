a332490d221b5b1c59c6bfd530c559e38632ccf9
Viktor Kuzmin <kvaster@gmail.com>
2023-08-07 00:42:37 +0300

    Fix Add...Capability functions - capabilities now added instead of replaced

--- pkg/driver/driver.go
+++ pkg/driver/driver.go
@@ -119,27 +119,18 @@ func (n *SeaweedFsDriver) Run() {
 	glog.Infof("stopped")
 }
 
-func (n *SeaweedFsDriver) AddVolumeCapabilityAccessModes(vc []csi.VolumeCapability_AccessMode_Mode) []*csi.VolumeCapability_AccessMode {
-	var vca []*csi.VolumeCapability_AccessMode
+func (n *SeaweedFsDriver) AddVolumeCapabilityAccessModes(vc []csi.VolumeCapability_AccessMode_Mode) {
 	for _, c := range vc {
 		glog.Infof("Enabling volume access mode: %v", c.String())
-		vca = append(vca, &csi.VolumeCapability_AccessMode{Mode: c})
+		n.vcap = append(n.vcap, &csi.VolumeCapability_AccessMode{Mode: c})
 	}
-	n.vcap = vca
-	return vca
 }
 
 func (n *SeaweedFsDriver) AddControllerServiceCapabilities(cl []csi.ControllerServiceCapability_RPC_Type) {
-	var csc []*csi.ControllerServiceCapability
-
 	for _, c := range cl {
 		glog.Infof("Enabling controller service capability: %v", c.String())
-		csc = append(csc, NewControllerServiceCapability(c))
+		n.cscap = append(n.cscap, NewControllerServiceCapability(c))
 	}
-
-	n.cscap = csc
-
-	return
 }
 
 func (d *SeaweedFsDriver) ValidateControllerServiceRequest(c csi.ControllerServiceCapability_RPC_Type) error {
