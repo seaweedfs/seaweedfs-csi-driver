6c5d2b47bc09c2f493d45ab383f51a9663bb4936
duanhongyi <duanhongyi@doopai.com>
2024-01-12 09:39:35 +0800

    chore(seaweedfs-csi-driver): delete unnecessary stage

--- pkg/driver/volume.go
+++ pkg/driver/volume.go
@@ -9,12 +9,11 @@ import (
 	"github.com/seaweedfs/seaweedfs/weed/pb/mount_pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"k8s.io/mount-utils"
 )
 
 type Volume struct {
 	VolumeId   string
-	StagedPath string
+	TargetPath string
 
 	mounter   Mounter
 	unmounter Unmounter
@@ -31,53 +30,29 @@ func NewVolume(volumeID string, mounter Mounter) *Volume {
 	}
 }
 
-func (vol *Volume) Stage(stagingTargetPath string) error {
-	// check whether it can be mounted
-	if isMnt, err := checkMount(stagingTargetPath); err != nil {
-		return err
-	} else if isMnt {
-		// try to unmount before mounting again
-		_ = mountutil.Unmount(stagingTargetPath)
-	}
-
-	if u, err := vol.mounter.Mount(stagingTargetPath); err == nil {
-		if vol.StagedPath != "" {
-			if vol.StagedPath == stagingTargetPath {
-				glog.Warningf("staged path is already set to %s for volume %s", vol.StagedPath, vol.VolumeId)
-			} else {
-				glog.Warningf("staged path is already set to %s and differs from %s for volume %s", vol.StagedPath, stagingTargetPath, vol.VolumeId)
-			}
-		}
-
-		vol.StagedPath = stagingTargetPath
-		vol.unmounter = u
-
-		return nil
-	} else {
-		return err
-	}
-}
-
-func (vol *Volume) Publish(stagingTargetPath string, targetPath string, readOnly bool) error {
+func (vol *Volume) Publish(targetPath string) error {
 	// check whether it can be mounted
 	if isMnt, err := checkMount(targetPath); err != nil {
 		return err
 	} else if isMnt {
-		// maybe already mounted?
+		// try to unmount before mounting again
+		_ = mountutil.Unmount(targetPath)
+	}
+
+	if u, err := vol.mounter.Mount(targetPath); err == nil {
+		if vol.TargetPath != "" {
+			if vol.TargetPath == targetPath {
+				glog.Warningf("target path is already set to %s for volume %s", vol.TargetPath, vol.VolumeId)
+			} else {
+				glog.Warningf("target path is already set to %s and differs from %s for volume %s", vol.TargetPath, targetPath, vol.VolumeId)
+			}
+		}
+		vol.TargetPath = targetPath
+		vol.unmounter = u
 		return nil
-	}
-
-	// Use bind mount to create an alias of the real mount point.
-	mountOptions := []string{"bind"}
-	if readOnly {
-		mountOptions = append(mountOptions, "ro")
-	}
-
-	if err := mountutil.Mount(stagingTargetPath, targetPath, "", mountOptions); err != nil {
+	} else {
 		return err
 	}
-
-	return nil
 }
 
 func (vol *Volume) Quota(sizeByte int64) error {
@@ -98,33 +73,24 @@ func (vol *Volume) Quota(sizeByte int64) error {
 }
 
 func (vol *Volume) Unpublish(targetPath string) error {
-	// Try unmounting target path and deleting it.
-	if err := mount.CleanupMountPoint(targetPath, mountutil, true); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (vol *Volume) Unstage(stagingTargetPath string) error {
-	glog.V(0).Infof("unmounting volume %s from %s", vol.VolumeId, stagingTargetPath)
+	glog.V(0).Infof("unmounting volume %s from %s", vol.VolumeId, targetPath)
 
 	if vol.unmounter == nil {
-		glog.Errorf("volume is not mounted: %s, path: %s", vol.VolumeId, stagingTargetPath)
+		glog.Errorf("volume is not mounted: %s, path: %s", vol.VolumeId, targetPath)
 		return nil
 	}
 
-	if stagingTargetPath != vol.StagedPath {
-		glog.Warningf("staging path %s differs for volume %s at %s", stagingTargetPath, vol.VolumeId, vol.StagedPath)
+	if targetPath != vol.TargetPath {
+		glog.Warningf("staging path %s differs for volume %s at %s", targetPath, vol.VolumeId, vol.TargetPath)
 	}
 
 	if err := vol.unmounter.Unmount(); err != nil {
-		glog.Errorf("error unmounting volume during unstage: %s, err: %v", stagingTargetPath, err)
+		glog.Errorf("error unmounting volume during unstage: %s, err: %v", targetPath, err)
 		return err
 	}
 
-	if err := os.Remove(stagingTargetPath); err != nil && !os.IsNotExist(err) {
-		glog.Errorf("error removing staging path for volume %s at %s, err: %v", vol.VolumeId, stagingTargetPath, err)
+	if err := os.Remove(targetPath); err != nil && !os.IsNotExist(err) {
+		glog.Errorf("error removing staging path for volume %s at %s, err: %v", vol.VolumeId, targetPath, err)
 		return err
 	}
 
