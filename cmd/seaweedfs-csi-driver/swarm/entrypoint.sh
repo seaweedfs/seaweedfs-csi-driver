9fe74adc881bc4643c6e635d4585bd1a363e8286
Gradlon von Känel <gradlon@integral-systems.ch>
2024-07-17 09:39:15 +0200

    remove

--- cmd/seaweedfs-csi-driver/swarm/entrypoint.sh
+++ cmd/seaweedfs-csi-driver/swarm/entrypoint.sh
@@ -7,6 +7,6 @@ fi
 
 NODE_ID=$(cat /node_hostname)
 C_WRITER=${C_WRITER:-32}
-CMD="/seaweedfs-csi-driver --filer=$FILER --nodeid=${NODE_ID} --endpoint=unix://run/docker/plugins/seaweed.sock --concurrentWriters=${C_WRITER} --dataCenter=${DATACENTER} --dataLocality=none --logtostderr --map.uid=${UID_MAP} --map.gid=${GID_MAP} --cacheCapacityMB=${CACHE_SIZE} --cacheDir=/tmp/seaweedFS/docker-csi" -enableAttacher=$ENABLE_ATTACHER" 
+CMD="/seaweedfs-csi-driver --filer=$FILER --nodeid=${NODE_ID} --endpoint=unix://run/docker/plugins/seaweed.sock --concurrentWriters=${C_WRITER} --dataCenter=${DATACENTER} --dataLocality=none --logtostderr --map.uid=${UID_MAP} --map.gid=${GID_MAP} --cacheCapacityMB=${CACHE_SIZE} --cacheDir=/tmp/seaweedFS/docker-csi" 
 
 exec $CMD
