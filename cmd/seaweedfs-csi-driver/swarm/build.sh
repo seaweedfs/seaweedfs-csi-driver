08ab2d58121b6604836ec6dd8a7eeaac02737f05
Gradlon von Känel <gradlon@integral-systems.ch>
2024-07-17 15:05:39 +0200

    added documentation

--- cmd/seaweedfs-csi-driver/swarm/build.sh
+++ cmd/seaweedfs-csi-driver/swarm/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-VERSION=${1:-latest}
-ARCH=${2:-linux/amd64}
-PLUGIN_NAME=${3:-swarm-csi-swaweedfs}
-PLUGIN_TAG=${4:-v1.2.0}
-PREFIX=${5:-gradlon}
+USAGE="Usage: ./build.sh [PREFIX] [VERSION] [TAG] [PLUGIN_NAME] [ARCH]"
+
+if [[ "$1" == "-h" ]]; then
+  echo "$USAGE"
+  exit 0
+fi
+
+VERSION=${2:-latest}
+ARCH=${5:-linux/amd64}
+PLUGIN_NAME=${4:-swarm-csi-swaweedfs}
+PLUGIN_TAG=${3:-v1.2.0}
+PREFIX=${1:-gradlon}
 
 docker build --platform ${ARCH} --build-arg BASE_IMAGE=chrislusf/seaweedfs-csi-driver:${VERSION} --build-arg ARCH=$ARCH -t seawadd-csi_tmp_img .
 mkdir -p ./plugin/rootfs
