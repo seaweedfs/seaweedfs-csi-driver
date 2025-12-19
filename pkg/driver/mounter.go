e06a188bce5caaa9155734e8a0903deeb40aa8bf
noobhappylife <aratar1991@hotmail.com>
2025-12-19 09:31:12 +0000

    Allow concurrentReaders in volume attributes

--- pkg/driver/mounter.go
+++ pkg/driver/mounter.go
@@ -134,6 +134,7 @@ func (m *mountServiceMounter) buildMountArgs(targetPath, cacheDir, localSocket s
 		"filer":              strings.Join(filers, ","),
 		"filer.path":         path,
 		"cacheCapacityMB":    strconv.Itoa(m.driver.CacheCapacityMB),
+		"concurrentReaders":  "",
 		"concurrentWriters":  strconv.Itoa(m.driver.ConcurrentWriters),
 		"map.uid":            m.driver.UidMap,
 		"map.gid":            m.driver.GidMap,
